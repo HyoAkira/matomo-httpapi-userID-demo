54a154c9f507cb5282f2e57ad9f01f809cc94cbb
hyo <hyosuke@yandex.com>
2025-03-03 14:53:00 +0300

    a

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ function setID(event) {
   event.preventDefault();
   const username = document.querySelector('#name').value;
 
-  fetch(`http://localhost:8080/matomo.php?uid=${username}&rec=1&idsite=1`).then(response => {
+  fetch(`http://localhost:8080/matomo.php?uid=${username}&rec=1&idsite=1&rand=1`).then(response => {
     if (response.ok) {
       console.log('ID set', username);
     }
@@ -15,3 +15,16 @@ btn.addEventListener('click', event => {
   setID(event);
 
 });
+
+
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function () {
+  var u = "//localhost:8080/";
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
+  _paq.push(['setSiteId', '1']);
+  var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+  g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+})();
